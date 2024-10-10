fe0408650485a4eac6d3f04f938a6515ffb55f2c
cliffamadeus <reconcliffy@gmail.com>
2024-10-10 13:13:00 +0800

    Added barchart child class

--- charts/chart.js
+++ charts/chart.js
@@ -64,5 +64,43 @@ class LineChart extends ChartCreator {
     }
 }
 
+class BarChart extends ChartCreator {
+    constructor(dataUrl) {
+        super(dataUrl);
+        this.barCtx = document.getElementById('barChart');
+    }
+
+    createCharts() {
+        this.createBarChart();
+    }
+
+    createBarChart() {
+        new Chart(this.barCtx, {
+            type: 'bar',
+            data: {
+                labels: this.chartData.labels,
+                datasets: [{
+                    label: '# of Votes',
+                    data: this.chartData.data,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+}
+
 const lineChartCreator = new LineChart('data.json');
 lineChartCreator.init();
+
+const barChartCreator = new BarChart('data.json');
+barChartCreator.init();
+
+console.log(lineChartCreator.dataUrl);
+console.log(barChartCreator.dataUrl);
